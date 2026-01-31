ed4ceac63f84aeb81d1e9d1bef216f5673779640
johelmunoz <johel.mnz@gmail.com>
2026-01-31 18:50:26 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
+
 const CACHE='noiseless-work-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
- self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])));
+  self.skipWaiting();
 });
 self.addEventListener('activate',e=>{
- e.waitUntil(self.clients.claim());
+  e.waitUntil(self.clients.claim());
 });
 self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
